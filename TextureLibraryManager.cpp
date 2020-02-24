af8aa728df905f355e596eb86f5c71f0247e7897
Rebecca Taylor <rebecca.lee.taylor@gmail.com>
2020-02-24 10:12:34 -0500

    Finished with making external class file

--- TextureLibraryManager.cpp
+++ TextureLibraryManager.cpp
@@ -1,4 +1,3 @@
-// TextureLibraryManager.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include "compare2Vectors.h"
 #include "FileList.h"
 #include "MfgFromString.h"
@@ -10,24 +9,22 @@
 
 namespace fs = std::filesystem;
 
-//fs::path g_dirProj = "G:/Projects/Scripting - TextureLibraryManager - 0.0.1/Test folders/Projects/";
-//fs::path g_dirLib = "G:/Projects/Scripting - TextureLibraryManager - 0.0.1/Test folders/_Texture Library/";
+std::string g_askTextProj = "Enter the path to the PROJECT folder\nFor first time use, use the root of ALL projects (This may take a while)";
+std::string g_askTextLib = "Enter the path to the LIBRARY folder";
 
 int main()
 {
-
 	//user input project folder
-	std::cout << "Enter the path to the PROJECT folder" << std::endl << "For first time use, use the root of ALL projects (This may take a while)" << std::endl;
+	std::cout << g_askTextProj << std::endl;
 	std::string cin_dirProj;
 	std::getline(std::cin, cin_dirProj);
 	fs::path dirProj = cin_dirProj;
 	
 	//user input library folder
-	std::cout << "Enter the path to the LIBRARY folder" << std::endl;
+	std::cout << g_askTextLib << std::endl;
 	std::string cin_dirLib;
 	std::getline(std::cin, cin_dirLib);
 	fs::path dirLib = cin_dirLib;
-	std::cout << dirLib << std::endl;
 
 	FileList checkProj(dirProj), checkLib(dirLib);
 	std::vector<fs::path> dirList1 = checkProj.List();//list of files in project directory
@@ -35,5 +32,4 @@ int main()
 	
 	Compare2Vectors projLib(dirList1, dirList2, dirLib);//copies files into folders
 	projLib.Compare();
-
 }
